d1686df400fe24ed27c9e9bd7defd892476df9d2
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:13:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -39,18 +39,26 @@ class Owner
   end
 
   def walk_dogs
+    self.pets[:dogs].each { |dog| dog.mood = "happy"}
   end
 
   def play_with_cats
+    self.pets[:cats].each { |cat| cat.mood = "happy"}
   end
 
   def feed_fish
+    self.pets[:fishes].each { |fish| fish.mood = "happy"}
   end
 
   def sell_pets
+    self.pets.each do |species, animals|
+      animals.each { |animal| animal.mood = "nervous"}
+    animals.clear
+    end
   end
 
   def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dogs(s), and #{pets[:cats].count} cats(s)."
   end
 
 
