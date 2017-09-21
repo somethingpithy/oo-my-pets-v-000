6c572f18827f21ef1069c953c8483c59ad96b193
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:03:07 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def self.mood(new_mood)
+    @mood = new_mood
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @owners = []
+  
+  def self.reset_all
+    @owners.clear
+  end
+
+  def self.count
+    @owners.count
+  end
+
+  def initialize(species)
+    @species = species
+    @owners << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish
+  end
+
+  def buy_cat
+  end
+
+  def buy_dog
+  end
+
+  def walk_dogs
+  end
+
+  def play_with_cats
+  end
+
+  def feed_fish
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+  end
+
 end
