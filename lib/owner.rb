a9d4576dfd2cb38a999c8b850de6161b63903d0e
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:08:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,10 +30,12 @@ class Owner
     self.pets[:fishes] << Fish.new(name)
   end
 
-  def buy_cat
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
   end
 
-  def buy_dog
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
